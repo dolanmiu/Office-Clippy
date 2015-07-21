7868111403b8015dd969367f3ad32f599a66cbfc
Dolan Miu <dolan_miu@hotmail.com>
2015-07-21 12:15:19 +0100

    cleaned up tests

--- test/docx-test.js
+++ test/docx-test.js
@@ -8,17 +8,17 @@ describe('Testing creation of docx Office file', function () {
     'use strict';
 
     it('creating a simple blank paragraph in the file', function () {
-        var file = docx.create();
-        var paragraph = docx.createParagraph();
+        var file = docx.create(),
+            paragraph = docx.createParagraph();
         //var paragraph = docx.createParagraph("Hello World").bold().italics().fontSize(30).heading1();
         file.addParagraph(paragraph);
         assert.equal(true, file.document['w:document'][1]['w:body'][0].hasOwnProperty('w:p'));
     });
 
     it('creating a text filled paragraph in the file', function () {
-        var file = docx.create();
-        var paragraph = docx.createParagraph("Hello World");
-        var text = docx.createText().bold();
+        var file = docx.create(),
+            paragraph = docx.createParagraph("Hello World"),
+            text = docx.createText().bold();
         paragraph.addText(text);
         file.addParagraph(paragraph);
         //console.log(JSON.stringify(file.document));
@@ -26,18 +26,19 @@ describe('Testing creation of docx Office file', function () {
     });
 
     it('should create docx with heading1', function () {
-        var file = docx.create();
-        var paragraph = docx.createParagraph("Hello World").heading1().thematicBreak();
-        var text = docx.createText();
+        var file = docx.create(),
+            paragraph = docx.createParagraph("Hello World").heading1().thematicBreak(),
+            text = docx.createText();
         paragraph.addText(text);
         file.addParagraph(paragraph);
         //console.log(JSON.stringify(file.document));
+        assert.equal(true, file.document['w:document'][1]['w:body'][0].hasOwnProperty('w:p'));
     });
 
     it('should create docx with table', function () {
-        var file = docx.create();
-        var table = docx.createTable(3);
+        var file = docx.create(),
+            table = docx.createTable(3);
         file.addParagraph(table);
-        console.log(JSON.stringify(file.document));
+        //console.log(JSON.stringify(file.document));
     });
 });
